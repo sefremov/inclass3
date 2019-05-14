2dbef1350b659ba4a62ea9be4d776822c7fd3aac
sergefr <efremov-sg@wisenetlab.ru>
2019-05-14 10:46:22 +0300

    Implemented a function for reading the entire dataset

--- task1.py
+++ task1.py
@@ -1,17 +1,25 @@
-def read_file(file_name):
-    data = {}
+def read_file(file_name, abbr, data):
     with open(file_name, 'r', encoding='utf-8') as f:
         f.readline()
         for line in f:
             parts = line.split(',')
             date = parts[0]
-            row = [file_name, int(parts[1]), float(parts[10])]
+            row = [abbr, int(parts[1]), float(parts[10])]
             if date not in data:
                 data[date] = [row]
             else:
                 data[date].append(row)
+
+
+def read_all_data():
+    import os
+    data = {}
+    for file in os.scandir('data'):
+        if file.name.startswith('K'):
+            abbr = os.path.splitext(file.name)[0]
+            read_file(file.path, abbr, data)
     return data
 
 
-data = read_file('data/KCLT.csv')
+data = read_all_data()
 print(data)
